94b84b2c63a55c03181d7b29320bc04c43248744
freundf <75124564+freundf@users.noreply.github.com>
2026-09-26 17:06:10 +0200

    Use Sequence Pool instead of GlobalVars

--- newSong.lua
+++ newSong.lua
@@ -1,20 +1,3 @@
-local function loadSongTable()
-	local songStr = GetVar(GlobalVars(), "SongList") or ""
-	if songStr == "" then return {} end
-	
-	local songTable = {}
-	for item in string.gmatch(songStr, "([^,]+)") do
-		local it = string.match(item, "^%s*(.-)%s*$")
-		table.insert(songTable, it)
-	end
-	return songTable
-end
-
-local function saveSongTable(songTable)
-	local updatedStr = table.concat(songTable, ", ")
-	SetVar(GlobalVars(), "SongList", updatedStr)
-end
-
 local function findNextFreeSequence()
 	local seqPool = DataPool().Sequences
 	local freeIdx = 1001
@@ -25,15 +8,12 @@ local function findNextFreeSequence()
 end
 
 local function main()
-	local songTable = loadSongTable()
-	
 	local newSong = TextInput("Song Name:")
 	
 	if newSong and newSong ~= "" then
 		local freeIdx = findNextFreeSequence()
-		Cmd(string.format("Store Sequence %d \"%s\"", freeIdx, newSong))
-		table.insert(songTable, newSong)
-		saveSongTable(songTable)
+		Cmd(string.format("Copy Sequence 'Song Template' At Sequence %d '%s'", freeIdx, newSong))
+		Cmd(string.format("Select Sequence %d", freeIdx))
 	
 		Printf("Created Sequence " .. freeIdx .. " ('" .. newSong .. "') and updated SongList.")
 	end
